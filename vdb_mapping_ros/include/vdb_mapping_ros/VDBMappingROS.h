c5badaa7a1fbd446e730d24d10c13b42033a3229
Marvin Große Besselmann <grosse@fzi.de>
2024-08-09 16:56:35 +0000

    moved conversion methods to core lib

--- vdb_mapping_ros/include/vdb_mapping_ros/VDBMappingROS.h
+++ vdb_mapping_ros/include/vdb_mapping_ros/VDBMappingROS.h
@@ -159,42 +159,6 @@ public:
 
 
   /*!
-   * \brief Creates a compressed Bitstream as string from an input grid
-   *
-   * \param update Update grid
-   *
-   * \returns bitstream
-   */
-  std::string gridToStr(const typename VDBMappingT::UpdateGridT::Ptr update) const;
-
-  /*!
-   * \brief Creates a byte vector from an input grid
-   *
-   * \param update Update grid
-   *
-   * \returns bitstream
-   */
-  std::vector<uint8_t> gridToByteArray(const typename VDBMappingT::UpdateGridT::Ptr update) const;
-
-  /*!
-   * \brief Unpacks an update grid from a string
-   *
-   * \param msg Compressed Bitstream as std::string
-   *
-   * \returns Update Grid
-   */
-  typename VDBMappingT::UpdateGridT::Ptr strToGrid(const std::string& msg) const;
-
-  /*!
-   * \brief Unpacks an update grid from a vector of uint8_t
-   *
-   * \param msg Compressed Bitstream as std::vector<uint8_t>
-   *
-   * \returns Update Grid
-   */
-  typename VDBMappingT::UpdateGridT::Ptr byteArrayToGrid(const std::vector<uint8_t>& msg) const;
-
-  /*!
    * \brief Listens to map updates and creats a map from these
    *
    * \param update_msg Single map update from a remote mapping instance
